d0d9c138b8672c2e90399fd0142e8d1be9273495
Ryan Hunter <rhunter4@xula.edu>
2018-04-16 14:30:09 -0500

    initial try

---
+++ HOVLane.java
@@ -0,0 +1,13 @@
+
+public class HOVLane {
+    Integer num;
+
+    public HOVLane(int num) {
+        this.num = num;
+    }
+
+    @Override
+    public String toString() {
+        return "HOVLane: " + "num:" + num;
+    }
+}
